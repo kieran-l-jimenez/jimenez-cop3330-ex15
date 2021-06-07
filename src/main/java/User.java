a38fbc0fafc90e69afeaa8d3e083e111c1e6b1c8
Kieran Jimenez <kieran.l.jimenez@gmail.com>
2021-06-06 22:07:18 -0400

    Added Legal file to User.java

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,3 +1,7 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Kieran Jimenez
+ */
 public class User {
     public String username = "sampleUsername";
     public String password = "abc$123";
